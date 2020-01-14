a8524eb998c2929e76e5653f5cd2dacd9741d820
Edwin Ortiz <eortiz@udel.edu>
2020-01-13 21:32:01 -0500

    Pushing Project To Website
    Hope this works the first time.

---
+++ script.js
@@ -0,0 +1,23 @@
+function linkedIn() {
+   window.open('https://www.linkedin.com/in/edwin-j-ortiz/');
+}
+
+function gitHub() {
+   window.open('https://github.com/e-ortiz');
+}
+
+function openEmail() {
+   mailto:'eortiz@udel.edu';
+}
+
+function openNAMD() {
+   window.open('https://github.com/vipud/hpc/tree/master/f17');
+}
+
+function openEstuary() {
+   window.open('https://github.com/e-ortiz/JAVA-Estuary-Management-Game');
+}
+
+function openRockPaperScissors() {
+   window.open('https://codepen.io/eortiz333/full/OJPEwyZ');
+}
